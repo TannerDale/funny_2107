6344f79a5c1777518e13acc1ce8069c614ad6cb2
Tanner Dale <tanner@tannerdale.com>
2021-07-14 15:22:26 -0500

    Add iteration 3 tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -33,4 +33,32 @@ RSpec.describe User do
       expect(user.jokes).to eq([joke_1, joke_2])
     end
   end
+
+  context 'finding jokes' do
+    user_1 = User.new("Sal")
+    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+
+    user_1.learn(joke_1)
+    user_1.learn(joke_2)
+
+    it 'can tell a joke to a user to learn' do
+      user_2 = User.new("Ali")
+
+      user_1.tell(user_2, joke_1)
+      expect(user_2.jokes).to eq([joke_1])
+
+      user_1.tell(user_2, joke_2)
+      expect(user_2.jokes).to eq([joke_1, joke_2])
+    end
+
+    it 'can find a joke by id' do
+      user_2 = User.new("Ali")
+      user_1.tell(user_2, joke_1)
+      user_1.tell(user_2, joke_2)
+
+      expect(user_2.joke_by_id(22)).to eq(joke_1)
+      expect(user_2.joke_by_id(13)).to eq(joke_2)
+    end
+  end
 end
