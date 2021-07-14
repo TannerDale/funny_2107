897229e08e65953491745a81d1723123244d1e65
Tanner Dale <tanner@tannerdale.com>
2021-07-14 15:52:47 -0500

    Consolidate, add all joke helper method test

--- spec/open_mic_spec.rb
+++ spec/open_mic_spec.rb
@@ -30,9 +30,8 @@ RSpec.describe OpenMic do
 
     it 'can welcome and add performers' do
       open_mic.welcome(user_1)
-      expect(open_mic.performers).to eq([user_1])
-
       open_mic.welcome(user_2)
+
       expect(open_mic.performers).to eq([user_1, user_2])
     end
   end
@@ -41,7 +40,7 @@ RSpec.describe OpenMic do
     open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
     user_1 = User.new("Sal")
     user_2 = User.new("Ali")
-    
+
     open_mic.welcome(user_1)
     open_mic.welcome(user_2)
 
@@ -56,7 +55,16 @@ RSpec.describe OpenMic do
       expect(open_mic.repeated_jokes?).to be(false)
 
       user_1.learn(joke_1)
+
       expect(open_mic.repeated_jokes?).to be(true)
+    end
+
+    it 'knows all jokes' do
+      user_2.learn(joke_1)
+      user_2.learn(joke_2)
+      user_1.learn(joke_1)
+
+      expect(open_mic.all_jokes).to eq([joke_1, joke_2, joke_1])
     end
   end
 end
