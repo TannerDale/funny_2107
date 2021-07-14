7a6b837d251cba0d32049827d2e0bf0517af4bc2
Tanner Dale <tanner@tannerdale.com>
2021-07-14 15:31:37 -0500

    Add open mic tests

---
+++ spec/open_mic_spec.rb
@@ -0,0 +1,55 @@
+require './lib/user'
+require './lib/joke'
+require './lib/open_mic'
+
+RSpec.describe OpenMic do
+  context 'initialize' do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    it 'exists' do
+      expect(open_mic).to be_a(OpenMic)
+    end
+
+    it 'has a location' do
+      expect(open_mic.location).to eq("Comedy Works")
+    end
+
+    it 'has a date' do
+      expect(open_mic.date).to eq("11-20-18")
+    end
+
+    it 'has no performers by default' do
+      expect(open_mic.performers).to eq([])
+    end
+  end
+
+  context 'adding performers' do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    user_1 = User.new("Sal")
+    user_2 = User.new("Ali")
+
+    it 'can welcome and add performers' do
+      open_mic.welcome(user_1)
+      expect(open_mic.performers).to eq([user_1])
+
+      open_mic.welcome(user_2)
+      expect(open_mic.performers).to eq([user_1, user_2])
+    end
+  end
+
+  context 'jokes' do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    user_1 = User.new("Sal")
+    user_2 = User.new("Ali")
+
+    it 'can recognize repeated jokes' do
+      user_2.learn(joke_1)
+      user_2.learn(joke_2)
+
+      expect(open_mic.repeated_jokes?).to be(false)
+
+      user_1.learn(joke_1)
+      expect(open_mic.repeated_jokes?).to be(true)
+    end
+  end
+end
